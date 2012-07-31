6471708cbbc0c9b51e964b920d859abd619dc443
Sean Porter <portertech@gmail.com>
2012-07-30 18:43:51 -0700

    [sync] double quotes

--- lib/cloudfiles/container.rb
+++ lib/cloudfiles/container.rb
@@ -51,8 +51,8 @@ module CloudFiles
             :metadata => resphash,
             :container_read => response["x-container-read"],
             :container_write => response["x-container-write"],
-            :sync_to => response['x-container-sync-to'],
-            :sync_key => response['x-container-sync-key']
+            :sync_to => response["x-container-sync-to"],
+            :sync_key => response["x-container-sync-key"]
           }
         rescue ClientException => e
           raise CloudFiles::Exception::NoSuchContainer, "Container #{@name} does not exist" unless (e.status.to_s =~ /^20/)
